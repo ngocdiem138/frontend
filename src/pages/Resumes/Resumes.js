aee512cb74710fcff201570784f1a6bdb517d8bf
ngocdiem138 <89444580+ngocdiem138@users.noreply.github.com>
2022-10-08 14:35:51 +0700

    initial commit

---
+++ src/pages/Resumes/Resumes.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { ResumeProvider } from '../../Context';
+import Header from '../../component/Layouts/Header';
+import Navbar from '../../component/Layouts/Navbar';
+import Footer from '../../component/Layouts/Footer';
+import Main from '../../component/Main';
+import WebFont from 'webfontloader';
+import { ChakraProvider } from '@chakra-ui/react';
+function Resumes() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Pacifico', 'Poppins']
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <ChakraProvider>
+        <ResumeProvider>
+          <Navbar />
+          {/* <Header /> */}
+          <Main />
+          {/* <Footer /> */}
+        </ResumeProvider>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default Resumes;
